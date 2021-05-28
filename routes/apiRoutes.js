b124a8addbfbc3d3d4a573aa6a5672bc37f125d5
APB98 <borja.basolutions@gmail.com>
2021-05-28 01:58:49 -0500

    changes to the index js and addition of route files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,32 @@
+// First we need to require uniqid to give an unique id to every note that the user write
+const uniqid = require('uniqid');
+
+// Second we have to link our routes to a series of "data" sources
+// These data sources hold arrays os information on db,json.
+
+const dataBase = require('../db/db.json');
+
+console.log(dataBase.length);
+
+// ROUTING
+module.exports = (app) => {
+    // API GET Requests
+    // Below code handles when users visit a page
+    // In each of the below cases when a user visits a link
+    // They are shown a JSON of the data on the notes side of the screen
+
+    // GET /api/notes should read the db.json file and return all saved notes as JSON.
+    app.get('/api/notes', (req, res) => res.json(dataBase));
+
+    // API POST Request
+    // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+    app.post('api/notes', (req, res) => {
+        if (dataBase.length >= 1) {
+            dataBase.push(req.body);
+            res.json(true);
+            return req.body;
+        }
+    });
+
+}
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,21 @@
+// We need to include the path package to get the correct file path for our html files
+
+const path = require('path');
+
+// ROUTING FOR THE PATHS BETWEEN THE HTML DOCS
+
+module.exports = (app) => {
+    // => HTML GET REQUESTS
+    // Below code handles when users visit a page.
+    // In each of the below cases the user is shown an html page of content
+
+    // GET /notes should return the notes.html file.
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'));
+    });
+
+    // GET * should return the index.html file.
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+      });
+};
