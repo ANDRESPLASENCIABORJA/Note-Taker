d8593fc3483d36479ea1e0926e4b637e00d8310a
APB98 <borja.basolutions@gmail.com>
2021-08-07 17:47:59 -0500

    changes on the apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,3 @@
-// Require the router and the db file
-const save = require("../db/db.json");
-
 // Require file system to save the notes
 const fs = require('fs');
 
@@ -11,10 +8,13 @@ const { v4: uuidv4 } = require('uuid');
 // ROUTING
 module.exports = (app) => {
   // We are telling the route to read the db.json file and return what is stored there
-  app.get("/api/notes", (req, res) => res.json(save));
+  app.get("/api/notes", async (req, res) =>{
+    let jsonData = await JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+    res.json(jsonData);
+  });
 
   // Post route /api/notes should recieve a new note to save on the request body, add it to the db.json file and then return the new note to the client.
-  app.post("/api/notes", (req, res) => {
+  app.post("/api/notes", async (req, res) => {
     
     // Create a constant to recieve a new note and save it into the request body
     const newNote = req.body;
@@ -22,10 +22,15 @@ module.exports = (app) => {
     // Unique id for the new note
     newNote.id = uuidv4();
 
-    // Save the note into the json file
-    save.push(newNote);
+    // Save the note into the json file, first read it and then push the new data
+    let newJson = await JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    //Add the note to the json file
+    newJson.push(newNote);
+    // Tell file system to write it 
+    fs.writeFileSync('./db/db.json', JSON.stringify(newJson));
 
-    // Return the note to the client
+    // Send the new note to the client
     res.json(newNote);
+
   });
 };
