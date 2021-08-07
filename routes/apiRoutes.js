305b769ad4c9913c117c15ce61e7a7b3f9c51aff
APB98 <borja.basolutions@gmail.com>
2021-08-07 15:22:24 -0500

    functionallity of the apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,14 @@
 // Require the router and the db file
-const router = require("express").Router();
 const save = require("../db/db.json");
 
 // ROUTING
 
 module.exports = (app) => {
   // We are telling the route to read the db.json file and return what is stored there
-  router.get("/notes", (req, res) => res.json(save));
+  app.get("/api/notes", (req, res) => res.json(save));
 
   // Post route /api/notes should recieve a new note to save on the request body, add it to the db.json file and then return the new note to the client.
-  router.post("/notes", (req, res) => {
+  app.post("/api/notes", (req, res) => {
     // Create a constant to recieve a new note and save it into the request body
     const newNote = req.body;
 
@@ -19,5 +18,3 @@ module.exports = (app) => {
     res.json(newNote);
   });
 };
-
-
